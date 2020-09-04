c4bd6a9732dd001d8be0535fbfc1052e209617df
Brandon Maroni <brandon-maroni@lambdastudents.com>
2020-09-04 15:54:58 -0500

    imported useState and began to set up state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
 import './App.css';
+import TeamMembers from './components/TeamMembers';
+import MemberForm from './components/MemberForm';
 
 function App() {
-  return ();
+
+  const [ members, setMembers ] = useState([
+    {
+      name: 'name',
+      email: 'email',
+      role: 'role'
+    }
+  ]);
+
+  return (
+    <div>
+      <h1>Team</h1>
+      <TeamMembers />
+      <MemberForm />
+    </div>
+  );
 }
 
 export default App;
